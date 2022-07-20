9e80b8ff6ffb0cf55ca8d932e0893431025248ea
Tei Akpotosu-Nartey <rankingtei@gmail.com>
2022-07-20 15:00:41 +0000

    Initial Commit

---
+++ ToDoListApp/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoListApp
+//
+//  Created by Tei Akpotosu-Nartey on 7/19/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,170 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Tei Akpotosu-Nartey on 7/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    let floatingButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+        button.layer.cornerRadius = 30
+        button.backgroundColor = .gray
+        button.tintColor = .white
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.layer.shadowRadius = 10
+        button.layer.shadowOpacity = 0.4
+        
+        return button
+    }()
+    
+    private var models = [ToDoListItem]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        getAllItems()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+        view.addSubview(floatingButton)
+        configureAddButton()
+        
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        floatingButton.frame = CGRect(x: Int(view.frame.size.width) - 80,
+                                      y: Int(view.frame.size.height) - 100,
+                                      width: 60,
+                                      height: 60)
+    }
+    
+    
+    func configureAddButton(){
+        floatingButton.addTarget(self, action: #selector(didTapAdd), for: .touchUpInside)
+        
+    }
+    
+    
+    @objc private func didTapAdd(){
+        let alert = UIAlertController(title: "New Task", message: "Enter new Task", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
+            guard let field  = alert.textFields?.first, let text = field.text, !text.isEmpty else {
+                return
+            }
+            self?.createItem(name: text)
+        }))
+        present(alert, animated: true)
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return models.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.name
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = models[indexPath.row]
+        let sheet = UIAlertController(title: "Edit", message: nil,  preferredStyle: .actionSheet)
+        
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: {_ in
+            
+            let alert = UIAlertController(title: "Edit Task", message: "Change Task Name", preferredStyle: .alert)
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+                guard let field  = alert.textFields?.first, let newName = field.text, !newName.isEmpty else {
+                    return
+                }
+                self?.updateItem(item: item, newName: newName)
+            }))
+            self.present(alert, animated: true)
+            
+        }))
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: {[weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+        
+        present(sheet, animated: true)
+    }
+    
+    
+    func getAllItems (){
+        
+        do {
+            models = try context.fetch(ToDoListItem.fetchRequest())
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        catch {
+            //error
+        }
+    }
+    
+    func createItem(name: String){
+        let newItem  = ToDoListItem(context: context)
+        newItem.name = name
+        newItem.createdAt = Date()
+        getAllItems()
+        
+        do{
+            try context.save()
+            
+        }
+        catch{
+            //error
+        }
+    }
+    
+    func deleteItem(item: ToDoListItem){
+        context.delete(item)
+        do{
+            try context.save()
+            getAllItems()
+        }
+        catch{
+            //error
+        }
+    }
+    
+    func updateItem(item: ToDoListItem, newName: String){
+        item.name = newName
+        
+        do{
+            try context.save()
+            getAllItems()
+        }
+        catch{
+            //error
+        }
+    }
+}
+
